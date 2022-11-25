80d307cfc4eecff71d8ee046474f44d229551750
charly <charly@MacBook-Pro-de-charly-2.local>
2022-11-25 22:45:03 +0200

    Add deliveries controller & tests

---
+++ src/controllers/deliveries.controller.spec.ts
@@ -0,0 +1,201 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { DeliveriesController } from './deliveries.controller';
+import { ArticleService, FluxService, WebhooksService, DeliveryService, PrismaService } from '../services'
+import { Request } from 'express';
+import { HttpException } from '@nestjs/common';
+
+describe('Delivery Controller', () => {
+    let deliveryController: DeliveriesController;
+
+    let prismaService: PrismaService;
+    let articleService: ArticleService;
+    let fluxService: FluxService;
+    let webhookService: WebhooksService;
+    let deliveryService: DeliveryService;
+
+    prismaService = new PrismaService();
+    articleService = new ArticleService(prismaService);
+    fluxService = new FluxService(prismaService);
+    webhookService = new WebhooksService(prismaService);
+    deliveryService = new DeliveryService(prismaService);
+
+    beforeEach(async () => {
+        const app: TestingModule = await Test.createTestingModule({
+            controllers: [DeliveriesController],
+            providers: [DeliveryService, PrismaService],
+        }).compile();
+
+        deliveryController = app.get<DeliveriesController>(DeliveriesController);
+
+        prismaService.deliveries.deleteMany();
+        prismaService.articles.deleteMany();
+        prismaService.flux.deleteMany();
+        prismaService.webhooks.deleteMany();
+    });
+
+    afterAll(async () => {
+        prismaService.deliveries.deleteMany();
+        prismaService.articles.deleteMany();
+        prismaService.flux.deleteMany();
+        prismaService.webhooks.deleteMany();
+    });
+
+    describe('getAllDeliveriesContentOf', () => {
+        it('Empty request', async () => {
+            let request = { 
+                query: {}
+            } as Request
+            await expect(
+                deliveryController.getAllDeliveriesContentOf(request)
+                )
+                .rejects
+                .toThrow(HttpException);
+        });
+    
+        it('Wrong id', async () => {
+            let request = { 
+                query: {
+                    id: 1
+                }
+            } as unknown as Request;
+            expect(await deliveryController.getAllDeliveriesContentOf(request)).toEqual([]);
+        });
+
+        it('Wrong id', async () => {
+            let request = { 
+                query: {
+                    id: "1"
+                }
+            } as unknown as Request;
+            expect(await deliveryController.getAllDeliveriesContentOf(request)).toEqual([]);
+        });
+
+        it('Wrong id', async () => {
+            let request = { 
+                query: {
+                    id: "abc"
+                }
+            } as unknown as Request;
+            await expect(deliveryController.getAllDeliveriesContentOf(request))
+            .rejects
+            .toThrow(HttpException);
+        });
+
+        it('Wrong id', async () => {
+            let request = { 
+                query: {
+                    id: []
+                }
+            } as unknown as Request;
+            await expect(deliveryController.getAllDeliveriesContentOf(request))
+            .rejects
+            .toThrow(HttpException);
+        });
+
+        it('Webhook with no deliveries', async () => {
+            let webhook = await webhookService.createWebhook('url');
+            let request = { 
+                query: {
+                    id: webhook.id
+                }
+            } as unknown as Request;
+            expect(await deliveryController.getAllDeliveriesContentOf(request)).toEqual([]);
+        });
+
+        it('Webhook with no deliveries', async () => {
+            let webhook = await webhookService.createWebhook('url');
+            let flux = await fluxService.createFlux('url');
+            for (let i=0; i<2; i++) {
+                let article = await articleService.createArticle('title', flux.id);
+                await deliveryService.createDelevery(webhook.id, article.id);
+            }
+            let request = { 
+                query: {
+                    id: webhook.id
+                }
+            } as unknown as Request;
+            expect(await deliveryController.getAllDeliveriesContentOf(request))
+            .toEqual(await deliveryService.getDelevriesTo(webhook.id));
+        });
+    });
+    
+    describe('getAllDeliveryDestination', () => {
+        it('Empty request', async () => {
+            let request = { 
+                query: {}
+            } as Request
+            await expect(
+                deliveryController.getAllDeliveryDestination(request)
+                )
+                .rejects
+                .toThrow(HttpException);
+        });
+    
+        it('Wrong id', async () => {
+            let request = { 
+                query: {
+                    id: 1
+                }
+            } as unknown as Request;
+            expect(await deliveryController.getAllDeliveryDestination(request)).toEqual([]);
+        });
+
+        it('Wrong id', async () => {
+            let request = { 
+                query: {
+                    id: "1"
+                }
+            } as unknown as Request;
+            expect(await deliveryController.getAllDeliveryDestination(request)).toEqual([]);
+        });
+
+        it('Wrong id', async () => {
+            let request = { 
+                query: {
+                    id: "abc"
+                }
+            } as unknown as Request;
+            await expect(deliveryController.getAllDeliveryDestination(request))
+            .rejects
+            .toThrow(HttpException);
+        });
+
+        it('Wrong id', async () => {
+            let request = { 
+                query: {
+                    id: []
+                }
+            } as unknown as Request;
+            await expect(deliveryController.getAllDeliveryDestination(request))
+            .rejects
+            .toThrow(HttpException);
+        });
+
+        it('Webhook with no deliveries', async () => {
+            let flux = await fluxService.createFlux('url');
+            let article = await articleService.createArticle('title', flux.id);
+            let request = { 
+                query: {
+                    id: article.id
+                }
+            } as unknown as Request;
+            expect(await deliveryController.getAllDeliveryDestination(request)).toEqual([]);
+        });
+
+        it('Webhook with no deliveries', async () => {
+            let flux = await fluxService.createFlux('url');
+            let article = await articleService.createArticle('title', flux.id);
+            for (let i=0; i<2; i++) {
+                let webhook = await webhookService.createWebhook('url');
+                await deliveryService.createDelevery(webhook.id, article.id);
+            }
+            let request = { 
+                query: {
+                    id: article.id
+                }
+            } as unknown as Request;
+            expect(await deliveryController.getAllDeliveryDestination(request))
+            .toEqual(await deliveryService.getDelevriesOf(article.id));
+        });
+    });
+});
---
+++ src/controllers/deliveries.controller.ts
@@ -0,0 +1,51 @@
+import { Controller, Get, HttpException, HttpStatus, Req } from '@nestjs/common';
+import { DeliveryService } from '../services/deliveries.service';
+
+import { Articles, Webhooks } from '@prisma/client';
+
+import { Request } from 'express';
+
+@Controller('deliveries')
+export class DeliveriesController {
+
+    constructor(private readonly deliveryService: DeliveryService) {}
+    
+    @Get('articles')
+    async getAllDeliveryDestination(@Req() request: Request): Promise<Webhooks[]> {
+        if (!request.query.id)
+            throw new HttpException('An article id is required', HttpStatus.FORBIDDEN);
+
+            let id: number;
+
+            try {
+                id = parseInt(request.query.id as string);
+                if (isNaN(id)) {
+                    throw new HttpException('Id should be a number is required', HttpStatus.FORBIDDEN);
+                }
+            } catch {
+                throw new HttpException('Id should be a number is required', HttpStatus.FORBIDDEN);
+            }
+        
+        return this.deliveryService.getDelevriesOf(id);
+    }
+
+    @Get('webhooks')
+    async getAllDeliveriesContentOf(@Req() request: Request): Promise<Articles[]> {
+        if (!request.query.id)
+            throw new HttpException('A webhook id is required', HttpStatus.FORBIDDEN);
+
+        let id: number;
+
+        try {
+            id = parseInt(request.query.id as string);
+            if (isNaN(id)) {
+                throw new HttpException('Id should be a number is required', HttpStatus.FORBIDDEN);
+            }
+        } catch {
+            throw new HttpException('Id should be a number is required', HttpStatus.FORBIDDEN);
+        }
+
+        return this.deliveryService.getDelevriesTo(id);
+    }
+
+}
