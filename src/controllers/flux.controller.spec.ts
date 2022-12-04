1e1d833b29b46910689c1e6ecdb55c66d354b903
charly <charly@MacBook-Pro-de-charly-2.local>
2022-12-04 19:28:24 +0200

    Add flux controller with tests

---
+++ src/controllers/flux.controller.spec.ts
@@ -0,0 +1,299 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { FluxController } from './flux.controller'
+import { ArticleService, DeliveryService, WebhooksService, HooksService, FluxService, PrismaService } from '../services'
+import { Request } from 'express';
+import { HttpException } from '@nestjs/common';
+import { IncomingMessage, Server, ServerResponse } from 'http'
+
+describe('Flux controller tests', () => {
+    let fluxController: FluxController;
+
+    let prismaService = new PrismaService();
+    let articleService = new ArticleService(prismaService);
+    let fluxService = new FluxService(prismaService);
+    let webhookService = new WebhooksService(prismaService);
+    let deliveryService = new DeliveryService(prismaService);
+    let hookService = new HooksService(prismaService);
+
+    beforeEach(async () => {
+        const app: TestingModule = await Test.createTestingModule({
+            controllers: [FluxController],
+            providers: [FluxService, ArticleService, HooksService, DeliveryService, PrismaService],
+        }).compile();
+
+        fluxController = app.get<FluxController>(FluxController);
+
+        await prismaService.deliveries.deleteMany();
+        await prismaService.articles.deleteMany();
+        await prismaService.hooks.deleteMany();
+        await prismaService.webhooks.deleteMany();
+        await prismaService.flux.deleteMany();
+    });
+
+    afterAll(async () => {
+        await prismaService.deliveries.deleteMany();
+        await prismaService.articles.deleteMany();
+        await prismaService.hooks.deleteMany();
+        await prismaService.webhooks.deleteMany();
+        await prismaService.flux.deleteMany();
+    });
+    
+    describe('create', () => {
+        it('Missing url', async () => {
+            let request = {
+                body: {}
+            } as unknown as Request;
+
+            await expect(fluxController.update(request))
+            .rejects
+            .toThrow(HttpException);
+        });
+
+        it('Bad url', async () => {
+            let request = {
+                body: {
+                    url: 'Not an url'
+                }
+            } as unknown as Request;
+
+            await expect(fluxController.update(request))
+            .rejects
+            .toThrow(HttpException);
+        });
+
+        /*
+        it('Update when new article show up', async () => {
+            let server = new RssFluxTest();
+            server.listen(80);
+
+            let request = {
+                body: {
+                    url: 'http://localhost'
+                }
+            } as unknown as Request;
+            let flux = await fluxController.create(request);
+
+            server.addItem({
+                title: 'Article 1',
+                description: 'Great article',
+                link: 'toto.org'
+            })
+
+
+            console.log(new Date());
+            
+            await new Promise(resolve => setTimeout(resolve, 4000));
+
+            console.log(new Date());
+            expect((await articleService.getArticles()).length)
+            .toEqual(1);
+
+
+            server.close();
+        });
+        */
+    });
+
+    describe('getAll', () => {
+        it('Get all flux', async () => {
+            expect(await fluxController.getAll())
+            .toEqual(await fluxService.getAllFlux());
+
+            for (let i=0; i<2; i++)
+                expect(await fluxController.getAll())
+                .toEqual(await fluxService.getAllFlux());
+        });
+    });
+
+    describe('delete', () => {
+        it('Missing id', async () => {
+            let request = {
+                body: {}
+            } as unknown as Request;
+
+            await expect(fluxController.delete(request))
+            .rejects
+            .toThrow(HttpException);
+        });
+
+        it('Wrong id', async () => {
+            let request = {
+                body: {
+                    id: -1
+                }
+            } as unknown as Request;
+
+            await expect(fluxController.delete(request))
+            .rejects
+            .toThrow(HttpException);
+        });
+
+        it('Unknow id', async () => {
+            let request = {
+                body: {
+                    id: 'abc'
+                }
+            } as unknown as Request;
+
+            await expect(fluxController.delete(request))
+            .rejects
+            .toThrow(HttpException);
+        });
+
+        it('Remove flux and everything related to it', async () => {
+            let flux = await fluxService.createFlux('url');
+            let webhook = await webhookService.createWebhook('url');
+            let article = await articleService.createArticle('toto', flux.id);
+
+            await hookService.create_hook(flux.id, webhook.id);
+            await deliveryService.createDelevery(webhook.id, article.id);
+
+            let request = {
+                body: {
+                    id: flux.id
+                }
+            } as unknown as Request;
+
+            await fluxController.delete(request);
+
+            expect((await fluxService.getAllFlux()).length)
+            .toEqual(0);
+
+            expect((await articleService.getArticlesSendedBy(flux.id)).length)
+            .toEqual(0);
+
+            expect((await deliveryService.getDelevriesTo(webhook.id)).length)
+            .toEqual(0);
+
+            expect((await hookService.get_hooked(webhook.id)).length)
+            .toEqual(0);
+        });
+    });
+
+    describe('update', () => {
+        it('Missing id', async () => {
+            let request = {
+                body: {
+                    url: 'http://toto.org'
+                }
+            } as unknown as Request;
+
+            await expect(fluxController.update(request))
+            .rejects
+            .toThrow(HttpException);
+        });
+
+        it('Missing url', async () => {
+            let flux = await fluxService.createFlux('url');
+
+            let request = {
+                body: {
+                    id: flux.id
+                }
+            } as unknown as Request;
+
+            await expect(fluxController.update(request))
+            .rejects
+            .toThrow(HttpException);
+        });
+
+        it('Wrong id', async () => {
+            let request = {
+                body: {
+                    id: 'abc'
+                }
+            } as unknown as Request;
+
+            await expect(fluxController.update(request))
+            .rejects
+            .toThrow(HttpException);
+        });
+
+        it('Wrong url', async () => {
+            let flux = await fluxService.createFlux('url');
+
+            let request = {
+                body: {
+                    id: flux.id,
+                    url: 'Not an url'
+                }
+            } as unknown as Request;
+
+            await expect(fluxController.update(request))
+            .rejects
+            .toThrow(HttpException);
+        });
+
+        it('Unknow id', async () => {
+            let request = {
+                body: {
+                    id: '-1',
+                    url: 'http://toto.org'
+                }
+            } as unknown as Request;
+
+            await expect(fluxController.update(request))
+            .rejects
+            .toThrow(HttpException);
+        });
+
+        it('Good id and good url', async () => {
+            const new_url = 'http://toto.org';
+
+            let flux = await fluxService.createFlux('url');
+
+            let request = {
+                body: {
+                    id: flux.id,
+                    url: new_url
+                }
+            } as unknown as Request;
+
+            await fluxController.update(request);
+
+            expect((await fluxService.getFlux(flux.id)).url)
+            .toEqual(new_url);
+        });
+    });
+});
+
+type item = { title: string, 
+    description?: string, 
+    link?: string };
+
+class RssFluxTest extends Server {
+
+    private items: Array<item> = [];
+
+    constructor() {
+        super()
+        this.on('request', this.requestListener);
+    }
+
+    addItem(item: item) {
+        this.items.push(item);
+    }
+
+    private itemsToString(): string {
+        let str = "";
+        for (let item of this.items) {
+            let itemStr = "<item>";
+            itemStr += '<title>' + item.title + '</title>';
+            itemStr += '<description>' + item.description + '</description>';
+            itemStr += '<link>' + item.link + '</link>';
+            itemStr += '</item>';
+            str += itemStr;
+        }
+        return str;
+    }
+
+    private requestListener(request: IncomingMessage, response: ServerResponse) {
+        //response.setHeader('content-type', '')
+
+        response.write(
+            "<?xml version=\"1.0\"?>\n<rss version=\"2.0\"><channel><title>Test Rss Flux</title>" + this.itemsToString() + "</channel></rss>"
+        );
+
+        response.end();
+    }
+}
---
+++ src/controllers/flux.controller.ts
@@ -0,0 +1,175 @@
+import { Controller, Delete, Get, HttpException, HttpStatus, Patch, Post, Req } from '@nestjs/common';
+import { FluxService } from '../services/flux.service';
+import { HooksService } from '../services/hooks.service';
+import { DeliveryService } from '../services/deliveries.service';
+import { ArticleService } from '../services/articles.service';
+
+import { Flux } from '@prisma/client';
+
+import { Request } from 'express';
+
+import RssFeedEmitter = require('rss-feed-emitter');
+import axios from 'axios';
+
+@Controller()
+export class FluxController {
+
+    private rssFlux: Map<number,NodeJS.Timer> = new Map();
+    private feeder: RssFeedEmitter = new RssFeedEmitter({ skipFirstLoad: true });
+
+    constructor(private readonly fluxService: FluxService,
+        private readonly hookService: HooksService,
+        private readonly devliveryService: DeliveryService,
+        private readonly articleService: ArticleService) {}
+
+    @Post()
+    async create(@Req() request: Request) {
+        if (!request.body.url)
+            throw new HttpException('A url is required', HttpStatus.FORBIDDEN);
+
+        // Url format check
+        try {
+            new URL(request.body.url);
+        } catch {
+            throw new HttpException('Wrong url', HttpStatus.FORBIDDEN);
+        }
+
+        let flux = await this.fluxService.createFlux(request.body.url);
+
+
+        let event = `flux${flux.id}`;
+
+        this.feeder.add({
+            url: flux.url,
+            refresh: 2000,
+            eventName: event
+        });
+
+        this.feeder.addListener(event, this.onNewItem(flux));
+
+        /*
+        this.rssFlux.set(flux.id, setInterval(async () => {
+            const fl = await this.fluxService.getFlux(flux.id);
+            const parser = new Parser();
+            const feed = await parser.parseURL(flux.url);
+            
+
+            // TODO: Get most recent articles (last_articles.length == feed.items.length)
+            const last_articles = await this.articleService.getArticlesSendedBy(fl.id);
+            const article_exist = (article) => last_articles.some((a) => a.title == article.title && a.sourceId == fl.id);
+
+            const webhooks = await this.hookService.get_hooks(fl.id);
+
+            if (webhooks.length >= 0)
+                feed.items.forEach(async article => {
+                    if (!article_exist(article)) {
+                        let created_article = await this.articleService.createArticle(
+                            article.title,
+                            fl.id,
+                            article.description,
+                            article.link
+                        );
+
+                        webhooks.forEach(webhook => {
+                            this.devliveryService.createDelevery(webhook.id, created_article.id);
+                            axios.post(webhook.url, article);
+                        });
+                    }
+                });
+        }, 1000));
+        */
+
+    }
+    
+    @Get()
+    async getAll(): Promise<Flux[]> {
+        return this.fluxService.getAllFlux();
+    }
+
+    @Delete()
+    async delete(@Req() request: Request) {
+        if (!request.body.id)
+            throw new HttpException('An id is required', HttpStatus.FORBIDDEN);
+
+        let flux_id: number = parseInt(request.body.id);
+        if (isNaN(flux_id))
+            throw new HttpException('Flux id has to be a number', HttpStatus.FORBIDDEN);
+
+        if (!await this.exist(flux_id))
+        throw new HttpException('This id doesn\'t exist', HttpStatus.FORBIDDEN);
+        
+        // Removing all hooks
+        let hooks = await this.hookService.get_hooks(flux_id);
+        for (let hook of hooks)
+            await this.hookService.delete_hook(flux_id, hook.id)
+
+        // Removing all articles and deliveries
+        let articles = await this.articleService.getArticlesSendedBy(flux_id);
+        
+        for (let article of articles)
+            await this.devliveryService.deleteDeleveriesOf(article.id);
+
+        await this.articleService.deleteArticlesOf(flux_id);
+
+        /*
+        clearInterval(this.rssFlux[flux_id]);
+        this.rssFlux.delete(flux_id);
+        */
+        let deleted_flux = await this.fluxService.deleteFlux(flux_id);
+        this.feeder.remove(deleted_flux.url);
+
+        return deleted_flux;
+    }
+
+    @Patch()
+    async update(@Req() request: Request) {
+        if (!request.body.id)
+            throw new HttpException('Missing id', HttpStatus.FORBIDDEN);
+        if (!request.body.url)
+            throw new HttpException('Missing url', HttpStatus.FORBIDDEN);
+
+        if (!await this.exist(request.body.id))
+            throw new HttpException('Wrong id', HttpStatus.FORBIDDEN);
+
+        try {
+            new URL(request.body.url);
+        } catch {
+            throw new HttpException('Wrong url', HttpStatus.FORBIDDEN);
+        }
+
+        const old_flux = await this.fluxService.getFlux(request.body.id);
+
+        this.feeder.remove(old_flux.url);
+        this.feeder.add({
+            url: request.body.url,
+            refresh: 2000,
+            eventName: `flux${old_flux.id}`
+        });
+
+        return await this.fluxService.updateFlux(request.body.id, request.body.url);
+    }
+
+    private async exist(id: number): Promise<boolean> {
+        return (await this.getAll()).some(flux => flux.id == id);
+    }
+
+    private onNewItem(flux: Flux) {
+        return async (item) => {
+            console.log('NEW ITEM!!!!!!!!!!!!');
+            
+            const created_article = await this.articleService.createArticle(
+                item.title,
+                flux.id,
+                item.description,
+                item.link
+            );
+            const webhooks = await this.hookService.get_hooks(flux.id);
+
+            webhooks.forEach(webhook => {
+                this.devliveryService.createDelevery(webhook.id, created_article.id);
+                axios.post(webhook.url, item);
+            });
+        };
+    }
+}
+
